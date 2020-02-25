90236080b8c8a1832266270a2d2c17a9df02974d
Admin <admin@Admins-MacBook-Pro-2.local>
2020-02-25 16:49:47 +0000

    console.log onClick output

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,27 +11,47 @@ class TodoList extends Component {
               event.preventDefault()
                 this.props.createTask(this.task.value)
             }}>
-              <input id="newTask" ref={(input) => this.task = input }type="text" className="form-control" placeholder="Add task..." required />
+              <input id="newTask"
+              ref={(input) => {
+                this.task = input
+              }}
+              type="text"
+              className="form-control"
+              placeholder="Add task..."
+              required />
               <input type="submit" hidden={true} />
             </form>
             <ul id="taskList" className="list-unstyled">
               {this.props.tasks.map((task, key) => {
                 return(
-                  <div className="taskTemplate" className="checkbox" key={key}>
+                  <div className="taskTemplate" className="checkbox" key={key} >
                     <label>
-                      <input type="checkbox" />
-                      <span className="content">{task.content}</span>
-                    </label>
-                  </div>
+                      <input
+                       type="checkbox"
+                       name={task.id}
+                       ref={(input) => {
+                         this.checkbox = input
+                         console.log('checkbox', this.checkbox)
+                       }}
+                       onClick={(event) => {
+                          //console.log()
+                         console.log(task.content)
+                         console.log(task.id)
+                         console.log(task.completed)
+                         //console.log(this.checkbox)
+                       }}/>
+                        <span className="content">{task.content}</span>
+                      </label>
+                    </div>
                 )
-
               })}
             </ul>
             <ul id="completedTaskList" className="list-unstyled">
             </ul>
           </div>
         );
+
       }
     }
 
-export default TodoList;
+    export default TodoList;
